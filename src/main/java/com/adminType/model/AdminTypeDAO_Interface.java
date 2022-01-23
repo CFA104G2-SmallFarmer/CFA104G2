3ec31a3b68dbed61e750a75d8bb630b738157523
AllenChan9453 <97660472+AllenChan9453@users.noreply.github.com>
2022-01-23 20:57:24 +0800

    Add files via upload

---
+++ src/main/java/com/adminType/model/AdminTypeDAO_Interface.java
@@ -0,0 +1,17 @@
+package com.adminType.model;
+
+import java.util.List;
+
+public interface AdminTypeDAO_Interface {
+
+	public void insert(AdminTypeVO adminTypeVO);
+
+	public void update(AdminTypeVO adminTypeVO);
+
+	public void delete(Integer admin_type_id);
+
+	public AdminTypeVO findByPrimaryKey(Integer admin_type_id);
+
+	public List<AdminTypeVO> getAll();
+
+}
