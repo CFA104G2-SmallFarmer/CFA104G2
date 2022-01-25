917d67727bee9a8dbef3fb03305ac545cb27a83b
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-01-26 02:09:43 +0800

    Add files via upload

--- src/main/java/com/projPic/model/ProjPicDAO_interface.java
+++ src/main/java/com/projPic/model/ProjPicDAO_interface.java
@@ -4,10 +4,11 @@ import java.util.*;
 
 public interface ProjPicDAO_interface {
     public void insert(ProjPicVO projPicVO);
+//    update感覺用不到的樣子
     public void update(ProjPicVO projPicVO);
-    public void delete(Integer proj_pic_id, Integer proj_id);
-    public ProjPicVO findByPrimaryKey(Integer proj_id );
-//    public List<ProjPicVO> getAll(Integer proj_id);//呈現某專案的所有圖片
+    public void delete(Integer proj_pic_id);
+    public ProjPicVO findByPrimaryKey(Integer proj_pic_id );//呈現某圖
+    public List<ProjPicVO> getAllSameProj(Integer proj_id);//呈現某專案的所有圖片
 //    public List<ProjPicVO> getAll(Map<String, String[]> map); 
 
 }
